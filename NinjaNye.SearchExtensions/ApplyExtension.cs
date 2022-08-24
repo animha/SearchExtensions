5469e88b616383aceb77d2ec7092f5acb024db8e
Alberto Milani <alberto.milani@otg.it>
2022-08-24 16:02:56 +0200

    Implemented workaround to avoid issue: https://github.com/ninjanye/SearchExtensions/issues/40 as suggested by FWest98

---
+++ NinjaNye.SearchExtensions/ApplyExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace NinjaNye.SearchExtensions
+{
+    /// <summary>
+    /// Added to normalize IQueryable to prevent "The source IQueryable doesn't implement IAsyncEnumerable" excpetion
+    /// https://github.com/ninjanye/SearchExtensions/issues/40
+    /// </summary>
+    public static class ApplyExtension
+    {
+        public static IQueryable<TSource> Apply<TSource, TProperty>(this QueryableSearchBase<TSource, TProperty> source) {
+            return source.Where(source.AsExpression());
+        }
+
+        public static IQueryable<TParent> Apply<TParent, TChild, TProperty>(this QueryableChildSearchBase<TParent, TChild, TProperty> source) {
+            return source.Where(source.AsExpression());
+        }
+    }
+}
